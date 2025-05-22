9fc46b8c2c60ece9b4c4b9496a249c0005063572
DESKTOP-AEGVQ5M\hp <aberi.amir@yahoo.com>
2025-05-22 17:18:50 +0330

    style: improve layout and main page UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,16 @@ import SelectedProducts from "@/components/SelectedProducts";
 import UserList from "@/components/UserList";
 export default function Home() {
   return (
-    <div className="">
-      <main className="">
+    <main className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="bg-gray-800 rounded-xl p-4 shadow-lg">
         <ProductList />
+      </div>
+      <div className="bg-gray-800 rounded-xl p-4 shadow-lg">
         <UserList />
+      </div>
+      <div className="bg-gray-800 rounded-xl p-4 shadow-lg">
         <SelectedProducts />
-      </main>
-    </div>
+      </div>
+    </main>
   );
 }
