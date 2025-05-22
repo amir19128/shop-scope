08e773027c1b998d90b2cd0444af6dd8b77a93c7
DESKTOP-AEGVQ5M\hp <aberi.amir@yahoo.com>
2025-05-22 14:50:12 +0330

    feat: use action in produc list and selected product list for add and remove product

--- src/components/SelectedProducts.tsx
+++ src/components/SelectedProducts.tsx
@@ -1,10 +1,30 @@
-import { SearchInput } from "./SearchInput";
+'use client';
+import { RootState } from '@/store';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeProduct } from '@/store/slices/selectedProductsSlice';
 
-const SelectedProducts = () => (
-    <section>
-        {/* Selected Products */}
-        <h2 className="text-xl font-semibold mb-2">محصولات انتخاب شده </h2>
-        <SearchInput />
-    </section>
-);
+const SelectedProducts = () => {
+    const selected = useSelector((state: RootState) => state.selectedProducts.items);
+    const dispatch = useDispatch();
+
+    return (
+        <section>
+            <h2 className="">Selected Products</h2>
+            {selected.length === 0 && <p className="">محصولی انتخاب نشده</p>}
+            <ul className="">
+                {selected.map((product) => (
+                    <li
+                        key={product.id}
+                        className=""
+                        onClick={() => dispatch(removeProduct(product.id))}
+                    >
+                        <h4 className="font-bold">{product.title}</h4>
+                        <p className="text-sm">${product.price}</p>
+                    </li>
+                ))}
+            </ul>
+        </section>
+    );
+};
+
 export default SelectedProducts;
