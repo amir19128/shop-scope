3ec585d92377403cc1f84403a3b9c42d63c6a448
DESKTOP-AEGVQ5M\hp <aberi.amir@yahoo.com>
2025-05-22 15:21:14 +0330

    feat: change search input component and add to product component

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
+'use client';
 
-export const SearchInput = () => {
-    return (
-        <>
-            search control
-            {/* search control */}
-        </>
-    )
+interface Props {
+  value?: string;
+  onChange?: (value: string) => void;
+  placeholder?: string;
 }
+
+const SearchInput = ({ value = '', onChange, placeholder = 'جستجو...' }: Props) => {
+  return (
+    <input
+      type="text"
+      value={value}
+      onChange={(e) => onChange?.(e.target.value)}
+      placeholder={placeholder}
+      className=""
+    />
+  );
+};
+
+export default SearchInput;
