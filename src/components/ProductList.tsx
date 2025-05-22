744151243b511e2bf0865eeaacb10f4407616dcb
DESKTOP-AEGVQ5M\hp <aberi.amir@yahoo.com>
2025-05-22 12:12:38 +0330

    feat: display product with RTK Query

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,10 +1,27 @@
+'use client';
+import { useGetProductsQuery } from "@/store/services/productApi";
 import { SearchInput } from "./SearchInput";
+const ProductList = () => {
+  const { data: products, isLoading, isError } = useGetProductsQuery();
 
-const ProductList = () => (
+  if (isLoading) return <p className="text-center">در حال بارگذاری محصولات...</p>;
+  if (isError) return <p className="text-center text-red-500">خطا در دریافت محصولات!</p>;
+
+  return (
     <section>
-        {/* product component */}
-        <h2 className="text-xl font-semibold mb-2">محصولات</h2>
-        <SearchInput />
+      <h2 className="text-xl font-semibold mb-2">Products</h2>
+      <ul className="">
+        {products?.map((product) => (
+          <li
+            key={product.id}
+          >
+            <span className="">{product.title}</span>
+            <span className="">${product.price}</span>
+          </li>
+        ))}
+      </ul>
     </section>
-);
+  );
+};
+
 export default ProductList;
