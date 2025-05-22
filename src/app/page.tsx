8a5014fb71fcb27f94d05e6c75a61da7b79c3ace
DESKTOP-AEGVQ5M\hp <aberi.amir@yahoo.com>
2025-05-22 13:39:46 +0330

    feat: display product with scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import ProductList from "@/components/ProductList";
 import SelectedProducts from "@/components/SelectedProducts";
 import UserList from "@/components/UserList";
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="container mx-auto p-4 grid gap-4 grid-cols-1 md:grid-cols-3 justify-items-center">
+    <div className="">
+      <main className="">
         <ProductList />
         <UserList />
         <SelectedProducts />
